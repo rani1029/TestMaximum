22c8d504b046e2d824616f66b07b99eadf3aead2
rani1029 <ranig1029@gmail.com>
2021-08-19 14:23:24 +0530

    uc1 with test case 1.1 1.2 1.3 msg added

--- TestMaximm/Program.cs
+++ TestMaximm/Program.cs
@@ -8,7 +8,7 @@ namespace TestMaximm
         {
             Console.WriteLine("welcome to Testmaximum program");
             int M = FindMximum.FindIntNumber(10, 9, 9);
-            //Console.WriteLine("maximum value for test case 1 is " + M);
+            Console.WriteLine("maximum value for test case 1 is " + M);
             //int M2 = FindMximum.FindIntNumber(8, 9, 7);
             //Console.WriteLine("maximum value for test case 2 is " + M2);
             //int M3 = FindMximum.FindIntNumber(10, 9, 69);
