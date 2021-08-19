348ea0bdc0c56fd4aafd371427a773bf936dd262
rani1029 <ranig1029@gmail.com>
2021-08-19 15:00:50 +0530

    uc2 of float with test case 1.1 1.2 1.3  added

--- TestMaximm/FindMximum.cs
+++ TestMaximm/FindMximum.cs
@@ -22,7 +22,30 @@ namespace TestMaximm
                 return thirdValue;
             }
             return firstValue;
+        }
+
+        public static float FindfloatNumber(float firstValue, float SecondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(thirdValue) > 0 || SecondValue.CompareTo(firstValue) >= 0 && SecondValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) > 0 && SecondValue.CompareTo(thirdValue) >= 0)
+            {
+                return SecondValue;
+
+            }
+            if (thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 || thirdValue.CompareTo(SecondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 || thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+
+
+
 
         }
+
     }
 }
+
--- TestMaximm/Program.cs
+++ TestMaximm/Program.cs
@@ -9,10 +9,12 @@ namespace TestMaximm
             Console.WriteLine("welcome to Testmaximum program");
             int M = FindMximum.FindIntNumber(10, 9, 9);
             Console.WriteLine("maximum value for test case 1 is " + M);
-            //int M2 = FindMximum.FindIntNumber(8, 9, 7);
-            //Console.WriteLine("maximum value for test case 2 is " + M2);
-            //int M3 = FindMximum.FindIntNumber(10, 9, 69);
-            //Console.WriteLine("maximum value for testcase 3 is " + M3);
+            int M2 = FindMximum.FindIntNumber(8, 9, 7);
+            Console.WriteLine("maximum value for test case 2 is " + M2);
+            int M3 = FindMximum.FindIntNumber(10, 9, 69);
+            Console.WriteLine("maximum value for testcase 3 is " + M3);
+            float F1 = FindMximum.FindfloatNumber(1.0F, 9.9F, 0.8F);
+            Console.WriteLine("maximum value for test case 1 is " + F1);
         }
     }
 }
