e813824e156542342b9b4148b5cb1c802610fed8
rani1029 <ranig1029@gmail.com>
2021-08-19 14:14:46 +0530

    welcome msg added

---
+++ TestMaximm/FindMximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximm
+{
+    public class FindMximum
+    {
+        public static int FindIntNumber(int firstValue, int SecondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(thirdValue) > 0 || SecondValue.CompareTo(firstValue) >= 0 && SecondValue.CompareTo(thirdValue) > 0 || firstValue.CompareTo(SecondValue) > 0 && SecondValue.CompareTo(thirdValue) >= 0)
+            {
+                return SecondValue;
+
+            }
+            if (thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 || thirdValue.CompareTo(SecondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 || thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+
+        }
+    }
+}
--- TestMaximm/Program.cs
+++ TestMaximm/Program.cs
@@ -2,11 +2,17 @@
 
 namespace TestMaximm
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Testmaximum program");
+            int M = FindMximum.FindIntNumber(10, 9, 9);
+            //Console.WriteLine("maximum value for test case 1 is " + M);
+            //int M2 = FindMximum.FindIntNumber(8, 9, 7);
+            //Console.WriteLine("maximum value for test case 2 is " + M2);
+            //int M3 = FindMximum.FindIntNumber(10, 9, 69);
+            //Console.WriteLine("maximum value for testcase 3 is " + M3);
         }
     }
 }
